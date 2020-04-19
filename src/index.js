b2bb30031d3e18621f0a74bf03883e7dc77976a4
Asgar Ali <asgaralipq@gmail.com>
2020-04-19 09:49:19 +0530

    arrow functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 function add(...nums){
-  console.log(nums)
+  
+  let total = nums.reduce(function(x,y){
+    return x+y;
+  })
+  console.log(total)
+
 }
 
 add(4,5,6,7,8)
+
+function add1(...nums){
+  
+  let total = nums.reduce((x,y) => x+y )
+  console.log(total)
+
+}
+
+add1(4,5,6,7,8)
