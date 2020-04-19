f07ac2410f492ad97c2ef04656831d82b2ef9ea7
Asgar Ali <asgaralipq@gmail.com>
2020-04-19 05:38:11 +0530

    objects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const person = {
+  name: 'Asgar';
+  walk() {},
+  talk() {}
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+person.talk();
+person.name = '';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const targetMember = 'nam e';
+person[targetMember.value] = 'Asgar';
